9601e5047da4d39c96564a68383dd9134ed1c414
Mauro-Pereira <mauropereira1096@gmail.com>
2024-10-26 16:57:32 -0300

    fix: adding task set in user entity

--- src/main/java/com/example/task_manager/entity/User.java
+++ src/main/java/com/example/task_manager/entity/User.java
@@ -1,5 +1,6 @@
 package com.example.task_manager.entity;
 
+import java.util.Set;
 import java.util.UUID;
 
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -15,57 +16,53 @@ public class User {
     private String email;
     private String password;
     private boolean isAdmin = false;
+    private Set<Task> tasks;
 
-    public User(UUID userID, String name, String email, String password, boolean isAdmin) {
+    public User(){}
+    
+    public User(UUID userID, String name, String email, String password, boolean isAdmin, Set<Task> tasks) {
         this.userID = userID;
         this.name = name;
         this.email = email;
         this.password = password;
         this.isAdmin = isAdmin;
+        this.tasks = tasks;
     }
-
-    public User(){
-
-    }
-
     public UUID getUserID() {
         return userID;
     }
-
     public void setUserID(UUID userID) {
         this.userID = userID;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public boolean isAdmin() {
         return isAdmin;
     }
-
     public void setAdmin(boolean isAdmin) {
         this.isAdmin = isAdmin;
+    }
+    public Set<Task> getTasks() {
+        return tasks;
+    }
+    public void setTasks(Set<Task> tasks) {
+        this.tasks = tasks;
     }
 
     
