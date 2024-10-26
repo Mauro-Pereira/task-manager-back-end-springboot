976715745328dba985526c0c7442eed6f6e69254
Mauro-Pereira <mauropereira1096@gmail.com>
2024-10-26 18:44:22 -0300

    feat: adding saveUser method in userService

--- src/main/java/com/example/task_manager/service/UserService.java
+++ src/main/java/com/example/task_manager/service/UserService.java
@@ -1,8 +1,32 @@
 package com.example.task_manager.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
+
+import com.example.task_manager.entity.User;
+import com.example.task_manager.exception.UserAlreadyExistsException;
+import com.example.task_manager.repository.UserRepository;
 
 @Service
 public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User saveUser(User user){
+
+        Optional<User> returnedUser = this.userRepository.findById(user.getUserID());
+
+        if(returnedUser.isPresent()){
+            throw new UserAlreadyExistsException("User Already Exists");
+        }
+
+        return this.userRepository.save(user);
+        
+    }
 
 }
