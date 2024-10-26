420a9eb688d97d62951d4ac11f9900617b3d2e06
Mauro-Pereira <mauropereira1096@gmail.com>
2024-10-26 15:11:01 -0300

    feat: adding id attribute in user entity

---
+++ src/main/java/com/example/task_manager/entity/Task.java
@@ -0,0 +1,8 @@
+package com.example.task_manager.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Task {
+
+}
--- src/main/java/com/example/task_manager/entity/User.java
+++ src/main/java/com/example/task_manager/entity/User.java
@@ -1,47 +1,78 @@
 package com.example.task_manager.entity;
 
+import java.util.UUID;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    private UUID userID;
     private String name;
     private String email;
     private String password;
-    private boolean isAdmin;
+    private boolean isAdmin = false;
 
-    public User(){
-        
-    }
-
-    public User(String name, String email, String password, boolean isAdmin) {
+    public User(UUID userID, String name, String email, String password, boolean isAdmin) {
+        this.userID = userID;
         this.name = name;
         this.email = email;
         this.password = password;
         this.isAdmin = isAdmin;
     }
+
+    public User(){
+
+    }
+
+    public UUID getUserID() {
+        return userID;
+    }
+
+    public void setUserID(UUID userID) {
+        this.userID = userID;
+    }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     public boolean isAdmin() {
         return isAdmin;
     }
+
     public void setAdmin(boolean isAdmin) {
         this.isAdmin = isAdmin;
     }
+
+    
 
 }
