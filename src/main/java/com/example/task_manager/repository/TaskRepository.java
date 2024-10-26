e3594de1a139df267d0987524fdb8a8642cfe963
Mauro-Pereira <mauropereira1096@gmail.com>
2024-10-26 16:38:44 -0300

    fix: task repository

--- src/main/java/com/example/task_manager/repository/TaskRepository.java
+++ src/main/java/com/example/task_manager/repository/TaskRepository.java
@@ -1,9 +1,15 @@
 package com.example.task_manager.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.task_manager.entity.Task;
+
 @Repository
-public interface TaskRepository extends MongoRepository{
+public interface TaskRepository extends MongoRepository<Task, UUID>{
+
+
 
 }
