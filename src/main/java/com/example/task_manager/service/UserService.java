ee20d50584851914fb31d95a46cbc521577a2663
Mauro-Pereira <mauropereira1096@gmail.com>
2024-10-26 18:56:41 -0300

    feat: adding getUser and deleteUser method in UserService file

--- src/main/java/com/example/task_manager/service/UserService.java
+++ src/main/java/com/example/task_manager/service/UserService.java
@@ -2,11 +2,13 @@ package com.example.task_manager.service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
 import com.example.task_manager.entity.User;
 import com.example.task_manager.exception.UserAlreadyExistsException;
+import com.example.task_manager.exception.UserNotFoundException;
 import com.example.task_manager.repository.UserRepository;
 
 @Service
@@ -32,6 +34,30 @@ public class UserService {
 
     public List<User> listUser(){
         return this.userRepository.findAll();
+    }
+
+    public User getUserById(UUID idUser){
+
+        Optional<User> returnedUser = this.userRepository.findById(idUser);
+
+        if(returnedUser.isEmpty()){
+            throw new UserNotFoundException("User not found");
+        }
+
+        return returnedUser.get();
+    }
+
+    public void deleteUser(UUID idUser){
+
+        Optional<User> returnedUser = this.userRepository.findById(idUser);
+
+        if(returnedUser.isEmpty()){
+            throw new UserNotFoundException("User not found");
+        }
+
+        this.userRepository.deleteById(returnedUser.get().getUserID());
+
+
     }
 
 }
