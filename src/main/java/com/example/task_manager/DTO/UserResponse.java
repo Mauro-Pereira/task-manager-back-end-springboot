8577937985082d9f1e254fa73e23ca3c06972ff1
Mauro-Pereira <mauropereira1096@gmail.com>
2024-10-29 18:56:10 -0300

    feat: add code

--- src/main/java/com/example/task_manager/DTO/UserResponse.java
+++ src/main/java/com/example/task_manager/DTO/UserResponse.java
@@ -1,5 +1,18 @@
 package com.example.task_manager.DTO;
 
+import java.util.Set;
+import java.util.UUID;
+
+
+import com.example.task_manager.entity.entity_enum.ROLE;
+
 public class UserResponse {
+
+    private UUID userID;
+    private String name;
+    private String email;
+    private boolean isAdmin = false;
+    private ROLE role;
+    private Set<UUID> tasks;
 
 }
