676e50f72f7fb2f5c8ea96bae3acdde14eb63a7b
Mauro-Pereira <mauropereira1096@gmail.com>
2024-10-30 12:30:51 -0300

    feat: add task response DTO

--- src/main/java/com/example/task_manager/DTO/TaskResponse.java
+++ src/main/java/com/example/task_manager/DTO/TaskResponse.java
@@ -1,5 +1,16 @@
 package com.example.task_manager.DTO;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.example.task_manager.entity.entity_enum.TASK_STATUS;
+
 public class TaskResponse {
+
+    private UUID taskId;
+    private String title;
+    private String description;
+    private LocalDateTime expirationDate;
+    private TASK_STATUS status; 
 
 }
