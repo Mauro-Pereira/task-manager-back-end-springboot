ccc6a41f5194f6cc4e62e37b98e718d4eb44a70c
Mauro-Pereira <mauropereira1096@gmail.com>
2024-10-31 18:13:46 -0300

    feat: start TaskController

--- src/main/java/com/example/task_manager/controller/TaskController.java
+++ src/main/java/com/example/task_manager/controller/TaskController.java
@@ -1,5 +1,18 @@
 package com.example.task_manager.controller;
 
+import com.example.task_manager.service.TaskService;
+import com.example.task_manager.service.UserService;
+
 public class TaskController {
+
+    private UserService userService;
+    private TaskService taskService;
+
+    public TaskController(UserService userService, TaskService taskService){
+        this.userService = userService;
+        this.taskService = taskService;
+    }
+
+    
 
 }
