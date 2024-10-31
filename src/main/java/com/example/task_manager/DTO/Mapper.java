6608a4f5961abbc99830df478d4bb460f1e1359a
Mauro-Pereira <mauropereira1096@gmail.com>
2024-10-31 11:21:51 -0300

    feat: adding userResquestToUser method in mapper class

--- src/main/java/com/example/task_manager/DTO/Mapper.java
+++ src/main/java/com/example/task_manager/DTO/Mapper.java
@@ -1,5 +1,18 @@
 package com.example.task_manager.DTO;
 
+import com.example.task_manager.entity.User;
+
 public class Mapper {
+
+    public static User userRequestToUser(UserRequest userRequest){
+
+        User newUser = new User();
+        newUser.setName(userRequest.getName());
+        newUser.setEmail(userRequest.getEmail());
+        newUser.setPassword(userRequest.getPassword());
+        newUser.setAdmin(userRequest.isAdmin());
+        return newUser;
+        
+    }
 
 }
