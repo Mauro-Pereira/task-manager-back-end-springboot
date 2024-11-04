a79cb6f96a52c9ba074cfcab2b36b0b5f207f17d
Mauro-Pereira <mauropereira1096@gmail.com>
2024-11-04 17:48:17 -0300

    feat: adding handleUserAlreadyExistsException method

--- src/main/java/com/example/task_manager/exception/GlobalExeceptionHandler.java
+++ src/main/java/com/example/task_manager/exception/GlobalExeceptionHandler.java
@@ -1,8 +1,25 @@
 package com.example.task_manager.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExeceptionHandler {
+
+    @ExceptionHandler({UserNotFoundException.class})
+    public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException exception) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler({UserAlreadyExistsException.class})
+    public ResponseEntity<Object> handleUserAlreadyExistsException(UserAlreadyExistsException exception) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(exception.getMessage());
+    }
 
 }
