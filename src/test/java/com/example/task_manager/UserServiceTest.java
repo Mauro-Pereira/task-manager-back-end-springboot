0084695bd90eca785412dfe27238d9e9998b86f0
Mauro-Pereira <mauropereira1096@gmail.com>
2024-11-13 15:13:21 -0300

    feat: adding Unit Test to the UserService class

--- src/test/java/com/example/task_manager/UserServiceTest.java
+++ src/test/java/com/example/task_manager/UserServiceTest.java
@@ -1,7 +1,12 @@
 package com.example.task_manager;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -55,7 +60,7 @@ public class UserServiceTest {
         this.task = new Task("6733ac5eb2822c4054d18e3f", "Task Test", "This is a test", dataTime, TASK_STATUS.EXPIRED);
         this.tasks = new HashSet<>();
         tasks.add(task.getTaskId());
-        this.user = new User("6733840f7389f61ad5276a55", "User", "User@email.com", "password", false, ROLE.USER, tasks);
+        this.user = new User("6733840f7389f61ad5276a55", "User", "user@email.com", "password", false, ROLE.USER, tasks);
 
     }
 
@@ -101,22 +106,22 @@ public class UserServiceTest {
     }
 
     @Test
-    void shouldReturnUserWhenUserIdIsPassaed(){
+    void shouldReturnUserWhenUserIdIsPassed(){
         String userId = "6733840f7389f61ad5276a55";
         when(this.userRepository.findUserById(userId)).thenReturn(Optional.of(user));
 
-        Optional<User> returnedUser = this.userRepository.findById(userId);
+        Optional<User> returnedUser = this.userRepository.findUserById(userId);
 
-        assertEquals(returnedUser.get(), user);
+        assertEquals(user, returnedUser.get());
         assertEquals(userId, returnedUser.get().getId());
-        verify(this.userRepository, times(1)).findById(userId);
+        verify(this.userRepository, times(1)).findUserById(userId);
     }
 
     @Test
     void shouldReturnUserNotFoundExceptionWhenUserNotExists(){
 
-        String userId = "6733840f7389f61ad5276a55";
-        when(this.userRepository.findUserById(userId)).thenReturn(Optional.of(user));
+        String userId = "999999999999999";
+        when(this.userRepository.findUserById(userId)).thenReturn(Optional.empty());
 
         UserNotFoundException returnedException = assertThrows(
             UserNotFoundException.class,
@@ -124,8 +129,87 @@ public class UserServiceTest {
         );
 
         assertEquals("User not found", returnedException.getMessage());
-        verify(this.userRepository, times(1)).findById(userId);
+        verify(userRepository, never()).findById(anyString());
 
+    }
+
+    @Test
+    public void deleteUserShouldDeleteUserAndTasksWhenUserExists() {
+        String userId = "6733840f7389f61ad5276a55";
+
+        when(userRepository.findUserById(userId)).thenReturn(Optional.of(user));
+
+        userService.deleteUser(userId);
+
+        verify(taskRepository, times(1)).deleteTaskById(task.getTaskId());
+        verify(userRepository, times(1)).deleteUserById(userId);
+    }
+
+    @Test
+    public void deleteUserShouldThrowExceptionWhenUserDoesNotExist() {
+        String userId = "999999999999999";
+
+        when(userRepository.findUserById(userId)).thenReturn(Optional.empty());
+
+        assertThrows(UserNotFoundException.class, () -> userService.deleteUser(userId));
+
+        verify(taskRepository, never()).deleteTaskById(anyString());
+        verify(userRepository, never()).deleteUserById(anyString());
+    }
+
+    @Test
+    public void updateUser_ShouldUpdateUserFields_WhenUserExists() {
+        String userId = "6733840f7389f61ad5276a55";
+        User existingUser = new User(userId, "User", "user@email.com", "password", false, ROLE.USER, Set.of("task1"));
+        User userRequest = new User(userId, "Updated User", "updated@email.com", "newpassword", true, ROLE.ADMIN, Set.of("task1"));
+
+        // Simula o comportamento do repositório
+        when(userRepository.findUserById(userId)).thenReturn(Optional.of(existingUser));
+        when(userRepository.save(any(User.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        // Executa o método updateUser
+        User updatedUser = userService.updateUser(userId, userRequest);
+
+        // Verifica se os campos foram atualizados
+        assertEquals("Updated User", updatedUser.getName());
+        assertEquals("updated@email.com", updatedUser.getEmail());
+        assertEquals("newpassword", updatedUser.getPassword());
+        assertTrue(updatedUser.isAdmin());
+        assertEquals(ROLE.ADMIN, updatedUser.getRole());
+
+        // Verifica se o método save foi chamado
+        verify(userRepository, times(1)).save(updatedUser);
+    }
+
+    @Test
+    public void updateUserShouldNotUpdateFieldsWhenRequestFieldsAreNullOrEmpty() {
+        String userId = "6733840f7389f61ad5276a55";
+        User userRequest = new User(userId, "", "", "", false, ROLE.USER, Set.of("task1"));
+
+        when(userRepository.findUserById(userId)).thenReturn(Optional.of(user));
+        when(userRepository.save(any(User.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        User updatedUser = userService.updateUser(userId, userRequest);
+
+        assertEquals("User", updatedUser.getName());
+        assertEquals("user@email.com", updatedUser.getEmail());
+        assertEquals("password", updatedUser.getPassword());
+        assertFalse(updatedUser.isAdmin());
+        assertEquals(ROLE.USER, updatedUser.getRole());
+
+        verify(userRepository, times(1)).save(updatedUser);
+    }
+
+    @Test
+    public void updateUserShouldThrowExceptionWhenUserDoesNotExist() {
+        String userId = "999999999999999";
+        User userRequest = new User(userId, "Updated User", "updated@email.com", "newpassword", true, ROLE.ADMIN, Set.of("task1"));
+
+        when(userRepository.findUserById(userId)).thenReturn(Optional.empty());
+
+        assertThrows(UserNotFoundException.class, () -> userService.updateUser(userId, userRequest));
+
+        verify(userRepository, never()).save(any(User.class));
     }
 
 }
